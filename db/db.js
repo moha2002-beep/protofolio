f4a4e099d5f171be220650500800170deec35245
Mohamed ahmed <amii1nahmed2002@gmail.com>
2024-12-12 16:12:53 +0000

    Configure db

--- db/db.js
+++ db/db.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 // MySQL Database Connection
-const db = mysql.createConnection({
+let db = null;
+if (process.env.JAWSDB_URL){
+  db = mysql.createConnection(process.env.JAWSDB_URL)  ;
+}
+else{
+db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
-
+}
 db.connect((err) => {
   if (err) {
     console.error("Database connection failed:", err.stack);
