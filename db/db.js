14022d36277114e585e3107e31bd2bf7b41508f6
Mohamed <mohamedahmed02211@outlook.com>
2025-01-30 19:28:24 +0000

    Add port parameter to db connection

--- db/db.js
+++ db/db.js
@@ -1,18 +1,15 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 // MySQL Database Connection
-let db = null;
-if (process.env.JAWSDB_URL){
-  db = mysql.createConnection(process.env.JAWSDB_URL)  ;
-}
-else{
+
 db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  ssl: {rejectUnauthorized: false}
 });
-}
 db.connect((err) => {
   if (err) {
     console.error("Database connection failed:", err.stack);
